f78e2ba4dbd6e8b36a107c4034f79898597b5f39
saravanpatidar <spatidar@technomatz.com>
2025-03-13 11:16:53 +0530

    server update

--- server/connectToDB.js
+++ server/connectToDB.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://spatidar78:spj7869@saravan.qcw47.mongodb.net/NewProject').then(()=>{
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log('DB connect Successfully')
 }).catch(err=>console.log('Something Occurred',err));
